0df60e391232a3688307c28580927cf1faa4764d
Hardik makwana <121673340+hardymak@users.noreply.github.com>
2023-06-11 20:18:47 +0530

    Add files via upload

---
+++ assessment/2_d_Matrix.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int row,col;
+
+	int a[2][3]={3,5,8,8,15,12};
+	int b[2][3]={1,3,5,7,9,11};
+	int c[2][3];
+	printf("\n---First Matrix---\n");
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<3;col++)
+		{
+			printf(" %d ",a[row][col]);
+		}
+		printf("\n");
+	}
+	printf("\n---Second Matrix---\n");
+		for(row=0;row<2;row++)
+	{
+		for(col=0;col<3;col++)
+		{
+			printf(" %d ",b[row][col]);
+		}
+		printf("\n");
+	}
+
+	printf("\n---Multiplication Matrix---\n");
+	for(row=0;row<2;row++)
+	{
+		for(col=0;col<3;col++)
+		{
+			c[row][col]=a[row][col]*b[row][col];
+			printf(" %d ",c[row][col]);
+		}
+		printf("\n");
+	}
+}
+
